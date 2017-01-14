5de4fe529ed932c7541fc6bbdaa4a1407d11fe69
sunnyskar <sunskar.dhanker@gmail.com>
2017-01-13 23:42:32 -0800

    added a little more

--- src/start.java
+++ src/start.java
@@ -6,33 +6,49 @@ public class start {
 
 	public static void main(String[] args) {
 		int points = 0;
+		//Introduction to game
 		input = new Scanner(System.in);
-
-		//start game with name 
 		System.out.println("Hello! Welcome to my game. What is your name?");
 		String playername = input.nextLine();
-		System.out.println("You entered: " + playername + ". Is this correct? If not too bad.");
-		
-		input = new Scanner(System.in);
-		//introducing controls
-		input = new Scanner(System.in);
-		System.out.println("Type up, down, right, or left to chose/move. Type up to continue");
-		String startgame = input.nextLine();
-		
-		if (startgame == "up") {
-			
-			//first question
-			System.out.println("Let's get started. You will need to choose a pokemon stronger than the opponents."); 
-			input = new Scanner(System.in);
-            System.out.println("You are going against a Piplup. To choose turtwig go up. To choose chimchar go left. To choose lilipup go right. Choose wisely ");
-            String firstanswer= input.nextLine();
-            if(firstanswer == "turtwig"){
-            	points = points++;
-            }
-            else{
-            	
-            }
+
+		//"checking" if name is right
+		String namecorrect;
+		do {
+			System.out.println("You entered: " + playername + ". Is this correct? If not too bad.");
+		namecorrect = input.nextLine();
 		}
-	
+		while (namecorrect != "yes");
+		// introducing controls
+		String startgame;
+		do {System.out.println("Type up, down, right, or left to chose/move. Type up to continue");
+		startgame = input.nextLine();
+				} 
+		while (startgame != "up");
+
+			// first question
+			System.out.println("Let's get started. You will need to choose a pokemon stronger than the opponents.");
+			System.out.println("You are going against a Piplup. To choose turtwig go up. To choose chimchar go left. To choose lilipup go right. Choose wisely ");
+			String firstanswer = input.nextLine();
+			switch(firstanswer) {
+			
+			case "up" :
+				points= points+2;
+				System.out.println("You use vine whip. Super Effective. Easy win. You gained 2 points");
+				break;
+			
+			case "left":
+				System.out.println("Opponent used bubble beam. Super effective. You lost");
+				break;
+			
+			case "right":
+				System.out.println("You barely won. You manage to get a point");
+				points++;
+				break;
+			
+			default:
+				System.out.println("next time check your spelling");
+			}
+		}
+
 	}
-}
+
