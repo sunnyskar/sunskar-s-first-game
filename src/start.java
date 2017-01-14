67a1e4cad64cf8029cfc0e1003bed72ad3f28d10
sunnyskar <sunskar.dhanker@gmail.com>
2017-01-14 11:04:14 -0800

    finish 1.0

--- src/start.java
+++ src/start.java
@@ -6,49 +6,139 @@ public class start {
 
 	public static void main(String[] args) {
 		int points = 0;
-		//Introduction to game
+		// Introduction to game
 		input = new Scanner(System.in);
 		System.out.println("Hello! Welcome to my game. What is your name?");
 		String playername = input.nextLine();
 
-		//"checking" if name is right
+		// "checking" if name is right
 		String namecorrect;
 		do {
 			System.out.println("You entered: " + playername + ". Is this correct? If not too bad.");
-		namecorrect = input.nextLine();
-		}
-		while (namecorrect != "yes");
+			namecorrect = input.nextLine();
+		} while (!namecorrect.equalsIgnoreCase("yes"));
 		// introducing controls
 		String startgame;
-		do {System.out.println("Type up, down, right, or left to chose/move. Type up to continue");
-		startgame = input.nextLine();
-				} 
-		while (startgame != "up");
+		do {
+			System.out.println("Type up, down, right, or left to chose/move. Type up to continue");
+			startgame = input.nextLine();
+		} while (!startgame.equalsIgnoreCase("up"));
 
-			// first question
-			System.out.println("Let's get started. You will need to choose a pokemon stronger than the opponents.");
-			System.out.println("You are going against a Piplup. To choose turtwig go up. To choose chimchar go left. To choose lilipup go right. Choose wisely ");
-			String firstanswer = input.nextLine();
-			switch(firstanswer) {
-			
-			case "up" :
-				points= points+2;
-				System.out.println("You use vine whip. Super Effective. Easy win. You gained 2 points");
-				break;
-			
-			case "left":
-				System.out.println("Opponent used bubble beam. Super effective. You lost");
-				break;
-			
-			case "right":
-				System.out.println("You barely won. You manage to get a point");
-				points++;
-				break;
-			
-			default:
-				System.out.println("next time check your spelling");
-			}
+		// first question
+		System.out.println("Let's get started. You will need to choose a pokemon stronger than the opponents.");
+		System.out.println("You are going against a Piplup");
+		System.out.println("To choose turtwig go up.");
+		System.out.println("To choose chimchar go left.");
+		System.out.println("To choose lilipup go right.");
+		System.out.println("Choose wisely.");
+		String firstanswer = input.nextLine();
+
+		switch (firstanswer.toLowerCase()) {
+
+		case "up":
+			points = points + 2;
+			System.out.println("You use vine whip. Super Effective. Easy win. You gained 2 points");
+			break;
+
+		case "left":
+			System.out.println("Opponent used bubble beam. Super effective. You lost");
+			break;
+
+		case "right":
+			System.out.println("You barely won. You manage to get a point");
+			points++;
+			break;
+
+		default:
+			System.out.println("next time check your spelling");
 		}
+		String secondanswer;
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
 
+		System.out.println("Your next question is about the first evolution of a pokemon.");
+		System.out.println("What is the first evolution for charizard");
+		System.out.println("To choose chimchar type up.");
+		System.out.println("To choose charmander type left.");
+		System.out.println("To choose turtwig type right.");
+		System.out.println("To chose chespin type down");
+		System.out.println("Choose wisely");
+		secondanswer = input.nextLine();
+		switch (secondanswer.toLowerCase()) {
+		case "up" :
+			System.out.println("WROOOOOOOONG");
+			break;
+		case "left":
+			System.out.println("Good Job!");
+			points++;
+			break;
+		case "right":
+			System.out.println("WROOOOOOOONG");
+			break;
+		case "down":
+			System.out.println("WROOOOOOOONG");
+			break;
+		default:
+			System.out.println("next time check your spelling");
+			
+		}
+		String thirdanswer;
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("What is the first evolution of raichu");
+		System.out.println("To choose pikachu type up");
+		System.out.println("To choose pulsi type left");
+		System.out.println("To choose pichu type right");
+		System.out.println("To choose Anish type down");
+		System.out.println("Choose wisely");
+		thirdanswer = input.nextLine();
+		switch (thirdanswer.toLowerCase()) {
+		case "up" :
+			System.out.println("WROOOOOOOONG");
+			break;
+		case "right":
+			System.out.println("Good Job!");
+			points++;
+			break;
+		case "left":
+			System.out.println("WROOOOOOOONG");
+			break;
+		case "down":
+			System.out.println("WROOOOOOOONG");
+			break;
+		default:
+			System.out.println("next time check your spelling");
+		}
+		String lastanswer;
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("The last question is...");
+		System.out.println();
+		System.out.println();
+		//387
+		System.out.println("What is the national pokedex entry number of turtwig?");
+		lastanswer = input.nextLine();
+		switch (lastanswer.toLowerCase()) {
+		case "387" :
+			System.out.println("well done");
+			points++;
+			break;
+		default:
+			System.out.println("NOPE");
+		}
+	 System.out.println("You finished my game");
+	 System.out.println();
+	 System.out.println();
+	 System.out.println();
+	 System.out.println();
+		
+		
+		System.out.println("Your points are: " + points + " out of 5");
 	}
-
+}
